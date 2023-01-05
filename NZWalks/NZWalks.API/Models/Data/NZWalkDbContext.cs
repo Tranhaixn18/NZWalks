590cae0835e91778007a4a3f37d1d9f7e7e93e44
Tranhaixn18 <haitran17042k@gmail.com>
2023-01-05 20:54:31 +0700

    Add Db

---
+++ NZWalks/NZWalks.API/Models/Data/NZWalkDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Models.Data
+{
+    public class NZWalkDbContext:DbContext
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
